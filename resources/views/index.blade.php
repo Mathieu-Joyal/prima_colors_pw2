8e7aa809cb435319312aca2ad237413cb9221974
CSTJ\0823152 <m-eve_@hotmail.com>
2023-09-14 15:05:40 -0400

    creation page accueil / image / structure

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,2 +1,43 @@
-<!DOCTYPE html>
+<x-layout titre="Festival Prima-Colors | Accueil">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/nav.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/footer.css') }}">
+    <main>
+        <x-nav />
+        {{-- <video src=""></video> --}
+        --}}
+        <blockquote>
+           Super cool festival blablabla
+        </blockquote>
 
+        <p>le festival est blabla</p>
+
+        <div class="section_activites">
+            <img src="img/accueil/activites.jpg" alt="">
+            <h3>Activités hautes en couleurs!</h3>
+            <p> description activités</p>
+            {{-- <a href="{{ route('activites.index') }}">Voir les activités!</a> --}}
+        </div> 
+
+        <div class="section_forfaits">
+            <img src="img/accueil/forfaits.jpg" alt="">
+            <h3>Vivez Prima-Colors sous toutes ses teintes !</h3>
+            <p> description forfaits</p>
+            {{-- <a href="{{ route('forfait.index') }}">Voir les forfaits!</a> --}}
+        </div>
+
+        {{-- <x-ban_concours></x-ban_concours> --}}
+
+
+        <div class="section_actualités">
+            <h3>Actualités de nos éditions antérieurs</h3>
+            {{-- <a href="{{ route('actualité.index') }}"></a><button>actualité 1</button>
+            <a href="{{ route('actualité.index') }}"></a><button>actualité 2</button>
+            <a href="{{ route('actualité.index') }}"></a><button>actualité 3</button> --}}
+        </div>
+
+        {{-- <x-ban_compte></x-ban_compte> --}}
+        {{-- <x-ban_billet></x-ban_billet> --}}
+        <x-footer></x-footer>
+    </main>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,21 @@
 <?php
 
-
+use App\Http\Controllers\AccueilController;
 use App\Http\Controllers\ActualiteController;
 use App\Http\Controllers\ActiviteController;
 use Illuminate\Support\Facades\Route;
 
 /*ACCUEIL*/
+Route::get("/", [AccueilController::class, 'index'])
+    ->name('accueil');
 
 /**PAGE ACTIVITÉS */
-Route::get('/actualites', [ActualiteController::class, 'index'])
+Route::get('/activites', [ActiviteController::class, 'index'])
 ->name('activites.index');
 
 /**PAGE ACTUALITÉS */
 Route::get('/actualites', [ActualiteController::class, 'index'])
-->name('actiualites.index');
+->name('actualites.index');
 
 /** PAGE À PROPOS*/
 /** PAGE BILLETERIE */
