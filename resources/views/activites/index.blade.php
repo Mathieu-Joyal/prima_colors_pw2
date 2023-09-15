44ee5169027fe856f59675975771d26ab479e700
shante <shantespeaces@gmail.com>
2023-09-15 13:28:46 -0400

    restructuration fichier scss

--- resources/views/activites/index.blade.php
+++ resources/views/activites/index.blade.php
@@ -1,182 +1,193 @@
 <x-layout titre="Les activites">
 
 
-   <x-nav />
-
+    <x-nav />
+    {{-- <x-hero/> --}}
     <main>
-       {{-- <x-hero/> --}}
-
-<x-nav/>
-{{-- <x-hero/> --}}
-
-    <h1>activites</h1>
-    <h2 class = "selected">Vendredi</h2>
-    <h2>Samedi</h2>
-    <h2>Dimanche</h2>
-    <h2>2023 | 10 | 13</h2>
-
-    <section class="conteneur-activites">
-        @foreach ($vendrediActivites as $activite)
-            <article class="activites vendredi">
-
-                <div class="conteneur-image">
-                    <img class="thumbnail" src="{{ $activite->image }}" alt="image de l'activite">
-                </div>
 
 
-                <div class="conteneur-titre">
 
-                    <div class="heur">
-                        <p>heure {{ $activite->heure }}</p>
+        <main>
+            <section class="conteneur-activites">
+                <h1>activites</h1>
+
+                <div class="conteneur-date">
+
+                    <div class="conteneur-jours">
+                        <h2 class="selected">Vendredi</h2>
+                        <h2>Samedi</h2>
+                        <h2>Dimanche</h2>
                     </div>
 
-                    <div class="titre">
-                        <p> titre {{ $activite->titre }}</p>
+                    <div class="conteneur-bordure">
+                        <div class="bordure"></div>
+                        <div>
+                            <h2>2023 | 10 | 13</h2>
+                        </div>
                     </div>
-                    <div class="endroit">
-                        <p>endroit {{ $activite->endroit }}</p>
-                    </div>
-
-                <div class="titre">
-                    <p>titre {{ $activite->titre }}</p>
-
                 </div>
 
-                <div class="date-publication">
-                    <p> heure {{ $activite->heure }}</p>
+                @foreach ($vendrediActivites as $activite)
+                    <article class="activites vendredi">
 
-                </div>
-                <div class="date-publication">
-                    <p>endroit {{ $activite->endroit }}</p>
-
-
-                </div>
+                        <div class="conteneur-image">
+                            <img class="thumbnail" src="{{ $activite->image }}" alt="image de l'activite">
+                        </div>
 
 
-                <div class="description">
-                    <p> description {{ $activite->description }}</p>
+                        <div class="conteneur-titre">
 
-                </div>
-            </article>
-        @endforeach
-        <button>boutton voir plus</button>
+                            <div class="heur">
+                                <p>heure {{ $activite->heure }}</p>
+                            </div>
 
-    </section>
-    {{-- <x-ban_concours /> --}}
-    <section class="conteneur-activites">
-        <h2>Vendredi</h2>
-        <h2>Samedi</h2>
-        <h2>Dimanche</h2>
-        <h2>date de samedi</h2>
-        @foreach ($samediActivites as $activite)
-            <article class="activites samedi">
+                            <div class="titre">
+                                <p> titre {{ $activite->titre }}</p>
+                            </div>
+                            <div class="endroit">
+                                <p>endroit {{ $activite->endroit }}</p>
+                            </div>
 
-                <div class="conteneur-image">
-                    <img class="thumbnail" src="{{ $activite->image }}" alt="image de l'activite">
-                </div>
+                            <div class="titre">
+                                <p>titre {{ $activite->titre }}</p>
 
+                            </div>
 
-                <div class="conteneur-titre">
+                            <div class="date-publication">
+                                <p> heure {{ $activite->heure }}</p>
 
-                    <div class="heur">
-                        <p>heure {{ $activite->heure }}</p>
-                    </div>
-
-                    <div class="titre">
-                        <p> titre {{ $activite->titre }}</p>
-                    </div>
-                    <div class="endroit">
-                        <p>endroit {{ $activite->endroit }}</p>
-                    </div>
-
-                <div class="titre">
-                    <p>titre {{ $activite->titre }}</p>
-
-                </div>
-
-                <div class="date-publication">
-                    <p> heure {{ $activite->heure }}</p>
-
-                </div>
-                <div class="date-publication">
-                    <p>endroit {{ $activite->endroit }}</p>
+                            </div>
+                            <div class="date-publication">
+                                <p>endroit {{ $activite->endroit }}</p>
 
 
-                </div>
+                            </div>
 
 
-                <div class="description">
+                            <div class="description">
+                                <p> description {{ $activite->description }}</p>
 
-                    <p> description {{ $activite->description }}</p>
+                            </div>
+                    </article>
+                @endforeach
+                <button>boutton voir plus</button>
 
-                    <p>  description {{ $activite->description }}</p>
+            </section>
+            {{-- <x-ban_concours /> --}}
+            <section class="conteneur-activites">
+                <h2>Vendredi</h2>
+                <h2 class="selected">Samedi</h2>
+                <h2>Dimanche</h2>
+                <h2>date de samedi</h2>
+                @foreach ($samediActivites as $activite)
+                    <article class="activites samedi">
 
-
-                </div>
-            </article>
-        @endforeach
-        <button>boutton voir plus</button>
-
-    </section>
-
-    {{-- <x-ban_compte /> --}}
-
-    <section class="conteneur-activites">
-        <h2>Vendredi</h2>
-        <h2>Samedi</h2>
-        <h2>Dimanche</h2>
-        <h2>date de dimanche</h2>
-        @foreach ($dimancheActivites as $activite)
-            <article class="activites dimanche">
-
-                <div class="conteneur-image">
-                    <img class="thumbnail" src="{{ $activite->image }}" alt="image de l'activite">
-                </div>
+                        <div class="conteneur-image">
+                            <img class="thumbnail" src="{{ $activite->image }}" alt="image de l'activite">
+                        </div>
 
 
-                <div class="conteneur-titre">
+                        <div class="conteneur-titre">
 
-                    <div class="heur">
-                        <p>heure {{ $activite->heure }}</p>
-                    </div>
+                            <div class="heur">
+                                <p>heure {{ $activite->heure }}</p>
+                            </div>
 
-                    <div class="titre">
-                        <p> titre {{ $activite->titre }}</p>
-                    </div>
-                    <div class="endroit">
-                        <p>endroit {{ $activite->endroit }}</p>
-                    </div>
+                            <div class="titre">
+                                <p> titre {{ $activite->titre }}</p>
+                            </div>
+                            <div class="endroit">
+                                <p>endroit {{ $activite->endroit }}</p>
+                            </div>
 
-                <div class="titre">
-                    <p> titre {{ $activite->titre }}</p>
+                            <div class="titre">
+                                <p>titre {{ $activite->titre }}</p>
 
-                </div>
+                            </div>
 
-                <div class="date-publication">
-                    <p> heure {{ $activite->heure }}</p>
+                            <div class="date-publication">
+                                <p> heure {{ $activite->heure }}</p>
 
-                </div>
-                <div class="date-publication">
-                    <p> endroit {{ $activite->endroit }}</p>
-
-
-                </div>
+                            </div>
+                            <div class="date-publication">
+                                <p>endroit {{ $activite->endroit }}</p>
 
 
-                <div class="description">
-                    <p> description {{ $activite->description }}</p>
+                            </div>
 
-                </div>
 
-            </article>
-        @endforeach
-        <button>boutton voir plus</button>
+                            <div class="description">
 
-    </section>
-    {{-- <x-ban_billet /> --}}
+                                <p> description {{ $activite->description }}</p>
 
-</main>
+                                <p> description {{ $activite->description }}</p>
 
-    <x-footer />
+
+                            </div>
+                    </article>
+                @endforeach
+                <button>boutton voir plus</button>
+
+            </section>
+
+            {{-- <x-ban_compte /> --}}
+
+            <section class="conteneur-activites">
+                <h2>Vendredi</h2>
+                <h2>Samedi</h2>
+                <h2 class="selected">Dimanche </h2>
+                <h2>date de dimanche</h2>
+                @foreach ($dimancheActivites as $activite)
+                    <article class="activites dimanche">
+
+                        <div class="conteneur-image">
+                            <img class="thumbnail" src="{{ $activite->image }}" alt="image de l'activite">
+                        </div>
+
+
+                        <div class="conteneur-titre">
+
+                            <div class="heur">
+                                <p>heure {{ $activite->heure }}</p>
+                            </div>
+
+                            <div class="titre">
+                                <p> titre {{ $activite->titre }}</p>
+                            </div>
+                            <div class="endroit">
+                                <p>endroit {{ $activite->endroit }}</p>
+                            </div>
+
+                            <div class="titre">
+                                <p> titre {{ $activite->titre }}</p>
+
+                            </div>
+
+                            <div class="date-publication">
+                                <p> heure {{ $activite->heure }}</p>
+
+                            </div>
+                            <div class="date-publication">
+                                <p> endroit {{ $activite->endroit }}</p>
+
+
+                            </div>
+
+
+                            <div class="description">
+                                <p> description {{ $activite->description }}</p>
+
+                            </div>
+
+                    </article>
+                @endforeach
+                <button>boutton voir plus</button>
+
+            </section>
+            {{-- <x-ban_billet /> --}}
+
+        </main>
+
+        <x-footer />
 
 </x-layout>
