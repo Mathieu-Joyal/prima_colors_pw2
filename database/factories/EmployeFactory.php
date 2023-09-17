691aada857c7533a86570e322503e88b566dfe1e
Mathieu-Joyal <mathieu_joyal@hotmail.com>
2023-09-17 16:15:53 -0400

    Ajout de commentaires pertinants

--- database/factories/EmployeFactory.php
+++ database/factories/EmployeFactory.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Str;
 class EmployeFactory extends Factory
 {
     /**
-     * Define the model's default state.
+     * Définir les propriétés par défaut du modèle Employe.
      *
      * @return array
      */
