7aa1df07d3dc73580e5d36dc92396493ccc6208f
CSTJ\0823152 <m-eve_@hotmail.com>
2023-09-20 16:42:47 -0400

    regl prob nav, epuration code, ban cmte/concours

--- resources/views/components/ban_concours.blade.php
+++ resources/views/components/ban_concours.blade.php
@@ -1,6 +1,12 @@
 <div class="section_concours">
-    <img src="img/accueil/concours.jpg" alt="">
-    <h3>CréARTives : Concours d'Art Visuel"</h3>
-    <p> description concours</p>
-    {{-- <a href="{{ route('concours.index') }}">Infos sur le concours</a> --}}
-</div> 
+    <img src="img/accueil/ban_concours.png" alt="">
+    <div class="carre_noir">
+        <h1>Concours</h1>
+        <h3>CréARTive: Concours d'Art Visuel</h3>
+        <a href="/concours" class="concours">Inscription à CréARTives 
+            <span class="material-icons">
+                double_arrow
+            </span>
+        </a>
+    </div>
+</div>
