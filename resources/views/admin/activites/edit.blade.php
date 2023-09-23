79a3d62e4c63e07363ee813a67b802375b6e45b8
shantespeaces <shantespeaces@gmail.com>
2023-09-23 16:26:56 -0400

    css formulaire et list activité

--- resources/views/admin/activites/edit.blade.php
+++ resources/views/admin/activites/edit.blade.php
@@ -3,7 +3,7 @@
         <header class="">
 
             <h2 class="">modifier une activité
-                </h2>
+            </h2>
         </header>
 
         {{-- MESSAGES --}}
@@ -12,48 +12,65 @@
                 {{ session('echec') }}</p>
         @endif --}}
 
-        <div class="">
+        <div class="form">
             {{-- FORMULAIRE --}}
-            <form class="" action="{{ route('admin.activites.store')}}" method="POST" enctype="multipart/form-data">
+            <form class="" action="{{ route('admin.activites.store') }}" method="POST" enctype="multipart/form-data">
                 @csrf
 
-                <div>
-                    {{-- TITRE --}}
-                    <label for="titre" class="">Titre</label>
-                    <div class="">
-
+                <div class="conteneur-grid">
+                    <!-- Titre -->
+                    <div class="grid-item">
+                        <label for="titre" class="grid-title">Titre</label>
                         <x-forms.erreur champ="titre" />
+                        <input id="titre" name="titre" type="text" autofocus class=" "
+                            value="{{ old('titre') ?? $activite->titre }}">
+                    </div>
 
-                        <input id="titre" name="titre" type="text" autofocus
-                            class=" " value="{{ old('titre') ?? $activite->titre}}">
+                    <!-- Date -->
+                    <div class="grid-item">
+                        <label for="date" class="grid-title">Date</label>
+                        <x-forms.erreur champ="date" />
+                        <input id="date" name="date" type="text" autofocus class=" "
+                            value="{{ old('date') ?? $activite->date }}">
+                    </div>
+
+                    <!-- Heure -->
+                    <div class="grid-item">
+                        <label for="heure" class="grid-title">Heure</label>
+                        <x-forms.erreur champ="heure" />
+                        <input id="heure" name="heure" type="text" autofocus class=" "
+                            value="{{ old('heure') ?? $activite->heure }}">
+                    </div>
+                    <!-- Endroit -->
+                    <div class="grid-item">
+                        <label for="endroit" class="grid-title">Endroit</label>
+                        <x-forms.erreur champ="endroit" />
+                        <input id="endroit" name="endroit" type="text" autofocus class=" "
+                            value="{{ old('endroit') ?? $activite->endroit }}">
                     </div>
                 </div>
 
-                <div>
-                    {{-- DÉSCRIPTION --}}
-                    <label for="description" class="">Déscription</label>
-                    <div>
-                        <x-forms.erreur champ="description" />
-                        <input id="description" name="description" type="text" autofocus
-                            class=" " value="{{ old('description') ?? $activite->description}}">
-                    </div>
+
+                <!-- Description -->
+                <div class=" grid-item description">
+                    <label for="description" class="grid-title">Description</label>
+                    <x-forms.erreur champ="description" />
+                    <input id="description" name="description" type="text" autofocus class=" "
+                        value="{{ old('description') ?? $activite->description }}">
                 </div>
 
-                <div>
-                      {{-- IMAGE --}}
-                    <label for="image" class="">Image</label>
-                    <div>
-                        <x-forms.erreur champ="image" />
-                        <input id="image" name="image" type="file"
-                            class=" " value="{{ old('image') ?? $activite->image}}">
-                    </div>
+                <!-- Image -->
+                <div class=" grid-item">
+                    <label for="image" class="grid-title">Image</label>
+
+                    <x-forms.erreur champ="image" />
+                    <input id="image" name="image" type="file" class=" "
+                        value="{{ old('image') ?? $activite->image }}">
                 </div>
 
                 {{-- SUBMIT --}}
-                <div>
-                    <input type="submit"
-                        class=""
-                        value="Modifier!">
+                <div class="btn-submit">
+                    <input type="submit"  value="Modifier!">
                 </div>
             </form>
 
@@ -61,6 +78,11 @@
             <p class="">
                 <a href="{{ route('admin.activites.index') }}" class="">Retour aux activités</a>
             </p>
+
+
+
+
+
 
 
 
