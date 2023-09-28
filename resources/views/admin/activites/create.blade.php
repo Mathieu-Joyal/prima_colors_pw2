ac0f68a3a0017312adc5ec72d74bc53de3f715ad
shante <shantespeaces@gmail.com>
2023-09-28 13:49:01 -0400

    select, date et heure form ajout

--- resources/views/admin/activites/create.blade.php
+++ resources/views/admin/activites/create.blade.php
@@ -40,28 +40,53 @@
 
                     <!-- Date -->
                     <div class="grid-item">
-
                         <label for="date" class="grid-title">Date</label>
-
                         <x-forms.erreur champ="date" />
-
-                        <input id="date" name="date" type="text" autofocus class=" "
-                            value="{{ old('date') }}">
+                        <input id="date" name="date" type="date" autofocus class=" " value="{{ old('date') }}">
                     </div>
+
+
+                    <script>
+                        document.addEventListener('DOMContentLoaded', function() {
+                            const dateInput = document.getElementById('date');
+
+                            // Define the range of allowed dates
+                            const startDate = new Date('2023-10-13');
+                            const endDate = new Date('2023-10-15');
+
+                            // Convert dates to string format YYYY-MM-DD
+                            const startDateString = startDate.toISOString().split('T')[0];
+                            const endDateString = endDate.toISOString().split('T')[0];
+
+                            dateInput.setAttribute('min', startDateString);
+                            dateInput.setAttribute('max', endDateString);
+                        });
+                    </script>
 
                     <!-- Heure -->
                     <div class="grid-item">
                         <label for="heure" class="grid-title">Heure</label>
                         <x-forms.erreur champ="heure" />
-                        <input id="heure" name="heure" type="text" autofocus class=" "
+                        <input id="heure" name="heure" type="time" autofocus class=" "
                             value="{{ old('heure') }}">
                     </div>
                     <!-- Endroit -->
+
                     <div class="grid-item">
                         <label for="endroit" class="grid-title">Endroit</label>
                         <x-forms.erreur champ="endroit" />
-                        <input id="endroit" name="endroit" type="text" autofocus class=" "
-                            value="{{ old('endroit') }}">
+                        <select id="endroit" name="endroit">
+                            <option value="Groove Gallery">Groove Gallery</option>
+                            <option value="Urban Beats Arena">Urban Beats Arena</option>
+                            <option value="Graffiti Groove Stage">Graffiti Groove Stage</option>
+                            <option value="ColorSplash Sound Hub">ColorSplash Sound Hub</option>
+                            <option value="Artistic Beats Pavilion">Artistic Beats Pavilion</option>
+                            <option value="Street Art Soundstage">Street Art Soundstage</option>
+                            <option value="Vibrant Vibes Arena">Vibrant Vibes Arena</option>
+                            <option value="Funky Graffiti Groove">Funky Graffiti Groove</option>
+                            <option value="The Creative Rhythm Zone">The Creative Rhythm Zone</option>
+                            <option value="Epic Urban Art Beats">Epic Urban Art Beats</option>
+                        </select>
                     </div>
                 </div>
 
@@ -92,10 +117,6 @@
                     </div>
                 </div>
             </form>
-
-
-
         </div>
     </div>
-
 </x-layout>
