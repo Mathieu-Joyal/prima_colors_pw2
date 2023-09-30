4265e9113dab5f12060e1ac8d7e95b29babb5dce
shantespeaces <shantespeaces@gmail.com>
2023-09-30 08:32:29 -0400

    ajouter animation hover activites

--- resources/views/activites/index.blade.php
+++ resources/views/activites/index.blade.php
@@ -2,9 +2,10 @@
 
 
     <x-header />
-    {{-- <x-hero/> --}}
+
 
     <main>
+        <x-hero image="img\hero.png"/>
         <h1>activitÉs</h1>
         <x-activites.jour :activites="$vendrediActivites" journee="vendredi" id="vendredi"/>
          <x-ban_concours />
@@ -17,64 +18,5 @@
 
     <x-footer />
 
-    <script>
-        //images glide onto page as we scroll
-        document.addEventListener('DOMContentLoaded', function() {
-            const observer = new IntersectionObserver(glide, {
-                threshold: 0.2
-            });
-            const activites = document.querySelectorAll('.activites');
-            const images = document.querySelectorAll('.conteneur-image');
-
-            function glide(articles) {
-                articles.forEach(function(article) {
-                    if (article.isIntersecting) {
-                        article.target.classList.add('visible');
-                        observer.unobserve(article.target);
-                    }
-                });
-            }
-
-            activites.forEach(function(activite) {
-                observer.observe(activite);
-            });
-
-            images.forEach(function(image) {
-                observer.observe(image);
-            });
-        });
-
-
-        //see more / less @button click
-        function voirPlus(button) {
-            const buttonText = button.innerText;
-            const parentSection = button.closest('section');
-
-            const articles = parentSection.querySelectorAll('.activites');
-
-            articles.forEach(function(article) {
-                article.classList.toggle('hidden');
-            });
-
-            button.innerText = buttonText === 'VOIR PLUS' ? 'VOIR MOINS' : 'VOIR PLUS';
-        }
-
-
-        // show description on click
-        function toggleDescription(element) {
-            const description = element.querySelector('.description');
-            const nextElement = element.nextElementSibling;
-
-            if (description && nextElement) {
-                if (description.style.display === 'none' || description.style.display === '') {
-                    description.style.display = 'block';
-                    nextElement.style.marginTop = description.clientHeight + 'px';
-                } else {
-                    description.style.display = 'none';
-                    nextElement.style.marginTop = '0';
-                }
-            }
-        }
-    </script>
 
 </x-layout>
--- resources/views/components/activites/jour.blade.php
+++ resources/views/components/activites/jour.blade.php
@@ -44,7 +44,7 @@
         <article class="activites vendredi  {{ $key >= 3 ? 'hidden' : '' }} " id={{$id}}>
 
             <div class="conteneur-image" onclick="toggleDescription(this)">
-                <img class="thumbnail" src="{{ $activite->image }}" alt="image de l'activite">
+                <img class="image-activite" src="{{ $activite->image }}" alt="image de l'activite">
 
                 <div class="description">
                     <p>{{ $activite->description }}</p>
@@ -79,3 +79,64 @@
     <button class="voir activite" onclick="voirPlus(this)">Voir plus</button>
 
 </section>
+
+<script>
+    //images glide onto page as we scroll
+    document.addEventListener('DOMContentLoaded', function() {
+        const observer = new IntersectionObserver(glide, {
+            threshold: 0.2
+        });
+        const activites = document.querySelectorAll('.activites');
+        const images = document.querySelectorAll('.conteneur-image');
+
+        function glide(articles) {
+            articles.forEach(function(article) {
+                if (article.isIntersecting) {
+                    article.target.classList.add('visible');
+                    observer.unobserve(article.target);
+                }
+            });
+        }
+
+        activites.forEach(function(activite) {
+            observer.observe(activite);
+        });
+
+        images.forEach(function(image) {
+            observer.observe(image);
+        });
+    });
+
+
+    //see more / less @button click
+    function voirPlus(button) {
+        const buttonText = button.innerText;
+        const parentSection = button.closest('section');
+
+        const articles = parentSection.querySelectorAll('.activites');
+
+        articles.forEach(function(article) {
+            article.classList.toggle('hidden');
+        });
+
+        button.innerText = buttonText === 'VOIR PLUS' ? 'VOIR MOINS' : 'VOIR PLUS';
+    }
+
+
+    // show description on click
+    function toggleDescription(element) {
+        const description = element.querySelector('.description');
+        const nextElement = element.nextElementSibling;
+
+        if (description && nextElement) {
+            if (description.style.display === 'none' || description.style.display === '') {
+                description.style.display = 'block';
+                nextElement.style.marginTop = description.clientHeight + 'px';
+            } else {
+                description.style.display = 'none';
+                nextElement.style.marginTop = '0';
+            }
+        }
+    }
+</script>
+
