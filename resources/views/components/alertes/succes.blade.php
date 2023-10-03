6f9b217aac6761c21629f1c9eea9e5e64a4dbeee
CSTJ\0453402 <mathieu_joyal@hotmail.com>
2023-10-03 12:43:20 -0400

    correction composante succes

--- resources/views/components/alertes/succes.blade.php
+++ resources/views/components/alertes/succes.blade.php
@@ -2,10 +2,8 @@
 
 @if (session($cle))
 
-    <p @class([
-        "absolute top-4 right-4 rounded-md p-2",
-        "bg-green-300 text-green-900" => $cle == "succes",
-    ])>
-        {{ session($cle) }}
-</p>
+    <div class="alerte_succes">
+        <p>{{ session($cle) }}</p>
+    </div>
+
 @endif
