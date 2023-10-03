6d35e6fb9d27b6e80d23cf220869ec070845411f
justme-meve <m-eve_@hotmail.com>
2023-10-03 15:28:12 -0400

    modification footer a voir par eric

--- resources/views/apropos/index.blade.php
+++ resources/views/apropos/index.blade.php
@@ -1,16 +1,58 @@
 <x-layout titre="Festival Prima-Colors | Qui sommes-nous?">
 
-    <x-header/>
+    <x-header />
 
-    <x-hero image="img\a-propos\hero.jpg" />
+    <x-hero image="img\a-propos\hero.png" />
 
     <div class="conteneur-a_propos">
 
-    <h1>À propos</h1>
+        <h1>À propos</h1>
 
         <section class="a_propos_description">
             <article>
-                <p>Milk kopi-luwak id dripper carajillo strong blue mountain ristretto, crema, whipped, macchiato dark aged, et breve, beans robusta id medium espresso shop. Plunger pot acerbic percolator cappuccino, cream id, sugar, affogato whipped white eu latte kopi-luwak cinnamon foam. Latte french press instant body, aroma wings milk, siphon at, lungo, chicory ristretto mug percolator shop white, saucer ut as siphon fair trade. Arabica, qui steamed, grinder plunger pot robusta froth macchiato galão mazagran extraction half and half turkish viennese whipped. Caramelization plunger pot, a to go, percolator at roast arabica in extraction latte decaffeinated beans. Fair trade, café au lait robust white, body, turkish acerbic, dripper, robusta qui, filter est instant strong sweet. Café au lait iced doppio aromatic coffee, single origin blue mountain pumpkin spice ut single origin americano brewed milk aged medium cappuccino redeye. Espresso at dark cappuccino grounds café au lait redeye ut eu dark caramelization americano filter. Aroma crema, caffeine, sugar americano, mug extra  french press, affogato café au lait body and lungo pumpkin spice macchiato whipped kopi-luwak caramelization as froth est cup redeye. Caffeine turkish blue mountain plunger pot cultivar so qui java, chicory body pumpkin spice ristretto sweet wings macchiato.</p>
+                <p class="titre">
+                    À Propos du Festival Prima-Colors <br>
+                </p>
+                <p>
+                    Le Festival Prima-Colors, établi depuis cinq ans au cœur de Toronto, incarne une fusion vivante
+                    d'art, de musique et de créativité. Cet événement emblématique est une célébration de l'expression
+                    artistique sous toutes ses formes et une ode à la diversité culturelle.
+                </p>
+                <p class="titre">
+                    Notre Histoire <br>
+                </p>
+                <p>
+                    Fondé en 2018 par un groupe de passionnés de l'art, le Festival Prima-Colors a rapidement pris
+                    racine dans le paysage culturel de Toronto. Depuis lors, il est devenu un rendez-vous annuel
+                    incontournable pour les amoureux de l'art, attirant des milliers de visiteurs chaque année.
+                </p>
+                <p class="titre">
+                    Notre Essence <br>
+                </p>
+                <p>
+                    Le Festival Prima-Colors est un lieu où l'art devient une expérience immersive, où les couleurs
+                    dansent avec la musique et où la créativité s'épanouit. Il célèbre la fusion d'expressions
+                    artistiques variées, de la peinture à la musique, de la danse à la sculpture, créant un mélange
+                    vivifiant de formes artistiques.
+                </p>
+                <p class="titre">
+                    Ce que Nous Représentons <br>
+                </p>
+                <p>
+                    Nous incarnons la diversité culturelle de Toronto, mettant en lumière des artistes locaux et
+                    internationaux. Notre festival offre une plateforme pour l'innovation, l'exploration et la
+                    découverte artistique. Chaque année, nous repoussons les frontières de la créativité pour inspirer
+                    et émerveiller notre public.
+                </p>
+                <p class="titre">
+                    Joignez-vous à Nous <br>
+                </p>
+                <p>
+                    Que vous soyez un créateur, un amateur d'art ou un curieux à la recherche d'inspiration, le Festival
+                    Prima-Colors vous invite à vous joindre à notre célébration artistique. Venez partager avec nous
+                    cette expérience unique où les couleurs, les sons et les émotions se fondent en une symphonie
+                    d'expression créative.
+                </p>
             </article>
         </section>
 
@@ -35,7 +77,11 @@
 
             <div class="conteneur_map">
 
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2887.225815322848!2d-79.38167382343364!3d43.64346995305966!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d4cb2b5935bf09%3A0x5842d0e7d5669410!2sScotiabank%20Arena!5e0!3m2!1sfr!2sca!4v1696270309860!5m2!1sfr!2sca" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="responsive-iframe"></iframe>
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2887.225815322848!2d-79.38167382343364!3d43.64346995305966!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d4cb2b5935bf09%3A0x5842d0e7d5669410!2sScotiabank%20Arena!5e0!3m2!1sfr!2sca!4v1696270309860!5m2!1sfr!2sca"
+                    width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade" style="border:0;" allowfullscreen="" loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade" class="responsive-iframe"></iframe>
             </div>
         </div>
 
@@ -43,6 +89,6 @@
 
     </div>
 
-    <x-footer/>
+    <x-footer />
 
 </x-layout>
--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,31 +1,34 @@
 <div class="contenu">
     <div class="contenu_footer">
-        <a href="/" class="item_footer">
-            <div class="menu_footer">Accueil
-            </div>
-        </a>
-        <a href="/forfaits" class="item_footer">
-            <div class="menu_footer">Billets
-            </div>
-        </a>
-        <a href="/activites" class="item_footer">
-            <div class="menu_footer">Activités
-            </div>
-        </a>
-        <a href="/concours" class="item_footer">
-            <div class="menu_footer">Concours
-            </div>
-        </a>
-       
-        <a href="/actualites" class="item_footer">
-            <div class="menu_footer">Actualités
-            </div>
-        </a>
-        <a href="/apropos" class="item_footer">
-            <div class="menu_footer">À&nbsp;Propos
-            </div>
-        </a>
-    
+
+        <div class="menu_footer">
+            <a href="/" class="item_footer">
+                <div class="menu_footer">Accueil
+                </div>
+            </a>
+            <a href="/forfaits" class="item_footer">
+                <div class="menu_footer">Billets
+                </div>
+            </a>
+            <a href="/activites" class="item_footer">
+                <div class="menu_footer">Activités
+                </div>
+            </a>
+            <a href="/concours" class="item_footer">
+                <div class="menu_footer">Concours
+                </div>
+            </a>
+
+            <a href="/actualites" class="item_footer">
+                <div class="menu_footer">Actualités
+                </div>
+            </a>
+            <a href="/apropos" class="item_footer">
+                <div class="menu_footer">À&nbsp;Propos
+                </div>
+            </a>
+        </div>
+
     </div>
 
     <img src="img/logo/logo.png" alt="" class="logo">
@@ -35,9 +38,9 @@
     <div class="info">
         <p class="adresse">Prima-Colors@info.ca</p>
         <p class="telephone">514-612-3636</p>
-            <img src="img/reseau/facebook/f_logo_RGB-Black_58.png" alt="" class="reseau-img">
-            <img src="img/reseau/instagram/Instagram_Glyph_Black.png" alt="" class="reseau-img">
-            <img src="img/reseau/youtube/okyoutube_social_icon_dark.png" alt="" class="reseau-img">
+        <img src="img/reseau/facebook/f_logo_RGB-Black_58.png" alt="" class="reseau-img">
+        <img src="img/reseau/instagram/Instagram_Glyph_Black.png" alt="" class="reseau-img">
+        <img src="img/reseau/youtube/okyoutube_social_icon_dark.png" alt="" class="reseau-img">
     </div>
 
 </div>
