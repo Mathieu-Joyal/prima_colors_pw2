dcd4ffde5477c63798ca6d32b87e88a86087c7e5
shante <shantespeaces@gmail.com>
2023-10-04 09:33:29 -0400

    corriger erreur compilation css

--- resources/views/components/bannieres/compte.blade.php
+++ resources/views/components/bannieres/compte.blade.php
@@ -2,10 +2,12 @@
     <div class="carre_blanc">
         <h1 class="titre_compte">Pour&nbsp;ne&nbsp;rien&nbsp;manquer!</h1>
         <h3 class="texte_compte">Vous désirez tout savoir sur le festival et ne rien manquer? <br> Créez vous un compte!</h3>
-        <a href="/compte" class="compte">Créer un compte 
+
+        <a href="/compte" class="compte">Créer un compte
             <span class="material-icons">
                 add
                 </span>
         </a>
+
     </div>
 </div>
