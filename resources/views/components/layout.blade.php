2400722fab911cc24ea21008b094cf4026714b57
justme-meve <m-eve_@hotmail.com>
2023-10-12 10:55:55 -0400

    insertition du google analytic

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,13 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://use.typekit.net/wct0vvp.css">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
-
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <title>{{ $titre }}</title>
-    <!-- <script async src="https://www.googletagmanager.com/gtag/js?id=UA-XXXXXXXXX-Y"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-TMK4KSGPMW"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
 
@@ -21,8 +15,14 @@
             dataLayer.push(arguments);
         }
         gtag('js', new Date());
-        gtag('config', 'UA-XXXXXXXXX-Y');
-    </script> -->
+        gtag('config', 'G-TMK4KSGPMW');
+    </script>
+    <link rel="stylesheet" href="https://use.typekit.net/wct0vvp.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <title>{{ $titre }}</title>
 
 </head>
 
